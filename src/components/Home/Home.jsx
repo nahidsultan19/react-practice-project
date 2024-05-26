24d7b822c751853d646da16b12104f21d6da46a5
Nahid Sultan <mac@MacBook-Pro.local>
2024-05-26 09:13:20 +0800

    initial project setup

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Welcome to react world!</h2>
+            <Navbar/>
+        </div>
+    );
+};
+
+export default Home;
